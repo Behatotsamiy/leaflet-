b780f9c0a45c619aea9777892fd51ed1a8ed3d15
beha_757 <batiyorov757@gmail.com>
2024-04-04 19:45:37 +0500

    we will start

---
+++ layerGroup.js
@@ -0,0 +1,10 @@
+
+const cities = L.layerGroup([test]);
+
+const parks = L.layerGroup([crownHill, rubyHill]);
+
+const shopsContainer = L.layerGroup([shops, shops2]);
+
+const storecont = L.layerGroup([store, store2]);
+
+const poolscont = L.layerGroup([pools, pools1, pools2]);
---
+++ markers.js
@@ -0,0 +1,19 @@
+/** @format */
+
+const test = L.marker([39.61, -105.02]).bindPopup("This is Littleton, CO.");
+
+const crownHill = L.marker([39.75, -105.09]).bindPopup(
+  "This is Crown Hill Park."
+);
+const rubyHill = L.marker([39.68, -105.0]).bindPopup("This is Ruby Hill Park.");
+
+const shops = L.marker([39.63, -105.22]).bindPopup("This is Littleton, CO.");
+const shops2 = L.marker([39.67, -105.12]).bindPopup("This is Littleton, CO.");
+
+const store = L.marker([39.43, -105.34]).bindPopup("This is Aplle store.");
+const store2 = L.marker([39.47, -105.36]).bindPopup("This is Aplle store.");
+
+
+const pools = L.marker([39.44, -105.45]).bindPopup("This is pool.");
+const pools1 = L.marker([39.44, -105.47]).bindPopup("This is pool.");
+const pools2 = L.marker([39.39, -105.45]).bindPopup("This is pool.");
